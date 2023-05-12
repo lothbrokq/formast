21ea17c81719d50f757df9b2b89e15fb191c0d7e
s200545 <s200545@student.dtu.dk>
2023-05-12 13:13:47 +0200

    Fixed issues with 'None', updated the ast algorithm functions so that they are consistent

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ pdm run python -m formast <args>
 
 How to run: 
 ```
+2>&1 | tee log.txt 
+
 pdm run regit -vv --repo "C:\Users\boran\OneDrive\DTU\BSc Thesis\babyrepos\onlinebookstore" -o "C:\Users\boran\OneDrive\DTU\BSc Thesis\babyrepos\onlinebookstore_ast10" -p '**\*.java' -m mapping.csv -- formast --writeast --overwrite {}
 
 
--- src/formast/__main__.py
+++ src/formast/__main__.py
@@ -1,12 +1,10 @@
 import click
 import os
 from tree_sitter import Language, Parser
-
+import sys
 import base64
 import hashlib
-
 import logging
-
 from pathlib import Path
 
 log = logging.getLogger(__name__)
@@ -20,6 +18,7 @@ log = logging.getLogger(__name__)
 #         '../../vendor/tree-sitter-java'
 #     ]
 # )
+
 language_file = Path(__file__).absolute().parent.parent.parent / 'build' / 'my-languages.so'
 JAVA_LANGUAGE = Language(str(language_file), 'java')
 
@@ -28,72 +27,96 @@ JAVA_LANGUAGE = Language(str(language_file), 'java')
 @click.option("--writeast", is_flag=True, help="Write the new file with the AST content")
 @click.option("--writerelativeast", is_flag=True, help="Write the AST content with relative positions")
 @click.option("--writecompast", is_flag=True, help="Write the new file with the compressed AST content and hashing")
+@click.option("--writecompastsort", is_flag=True, help="Write the new file with the compressed AST content and sorted hashing")
 @click.option("--overwrite", is_flag=True, help="Overwrite the original .java file with the new content")
 @click.option("-v", "--verbose", count=True, help="Increase output verbosity")
-@click.argument("file_path", type=click.Path(exists=True)) # i stedet for file path, tag stdin og kør i batch mode
+@click.argument("file_path", type=str) 
 
 
 ## Formast 
-def formast(file_path, writetoken, writeast, writerelativeast, writecompast, overwrite, verbose):
+def formast(file_path, writetoken, writeast, writerelativeast, writecompast, writecompastsort, overwrite, verbose):
 
     # initialize logging
     logging.basicConfig(level=verbose)
 
-    #log.info("Formast v0.1.0")
-
     # Load the language (assuming the language is created, if not run the commented code above)
     language_file = Path(__file__).absolute().parent.parent.parent / 'build' / 'my-languages.so'
-    #log.debug("Using language file: %s", language_file)
+    log.debug("Using language file: %s", language_file)
     JAVA_LANGUAGE = Language(str(language_file), 'java')
     parser = Parser()
     parser.set_language(JAVA_LANGUAGE)
-    #log.info("Sucessfully created parser.")
+
+    if (file_path == "-"):
+        while True:
+            line = sys.stdin.readline()
+            if not line:
+                break
+            process(Path(line.strip()), parser, overwrite, writetoken, writeast, writerelativeast, writecompast, writecompastsort)
+            log.info("processed %s" % line)
+            sys.stdout.write("ok\n")
+            sys.stdout.flush()
+    else:
+        process(Path(file_path), parser, overwrite, writetoken, writeast, writerelativeast, writecompast, writecompastsort)
+        
+def process(file_path, parser, overwrite, writetoken, writeast, writerelativeast, writecompast, writecompastsort):
 
     log.info(f"Processing {file_path}...")
     
     ## Handling options 
     # Token based
     if writetoken:  
-        tree = parse_java_file(parser, file_path)
-        save_tokenized_file(file_path, tree)
+        with open(file_path, 'r', encoding='utf-8') as f:
+            src = f.read()
+        tree = parser.parse(src.encode("utf8"))
+        with open(file_path.with_suffix(".ast"),'w', encoding='utf-8') as f:
+            for node in traverse(tree):
+                if node.child_count == 0:
+                    f.write(node.text.decode('utf-8'))
+                    f.write("\n")
+        #tree = parse_java_file(parser, file_path)
+        #save_tokenized_file(file_path, tree)
     # AST based
     elif writeast:
-        with open(file_path, "rb", encoding='utf-8') as f:
+        with open(file_path, "rb") as f:
             code = f.read()
         tree = parser.parse(code)
-        ast_code = process_tree(tree, code)
-        with open(file_path+".ast", 'w', encoding='utf-8') as f:
+        ast_code = process_tree_ast(tree)
+        with open(file_path.with_suffix(".ast"), 'w', encoding='utf-8') as f:
             f.write(ast_code)
     # Compressed AST based
     elif writecompast:
-        with open(file_path, "rb", encoding='utf-8') as f:
+        with open(file_path, "rb") as f:
             code = f.read()
         tree = parser.parse(code)
         ast_code = process_tree_comp(tree)
-        with open(file_path+".ast", 'w', encoding='utf-8') as f:
+        with open(file_path.with_suffix(".ast"), 'w', encoding='utf-8') as f:
+            f.write(ast_code)
+    # Sorted and compressed AST based
+    elif writecompastsort:
+        with open(file_path, "rb") as f:
+            code = f.read()
+        tree = parser.parse(code)
+        ast_code = process_tree_comp_sorted(tree)
+        with open(file_path.with_suffix(".ast"), 'w', encoding='utf-8') as f:
             f.write(ast_code)
     # Relative AST based
     elif writerelativeast:
-        with open(file_path, "rb", encoding='utf-8') as f:
+        with open(file_path, "rb") as f:
             code = f.read()
         tree = parser.parse(code)
-        ast_code = process_tree_relatively(tree, code)
-        with open(file_path+".ast", 'w', encoding='utf-8') as f:
+        ast_code = process_tree_ast_relatively(tree)
+        with open(file_path.with_suffix(".ast"), 'w', encoding='utf-8') as f:
             f.write(ast_code)
 
+    log.info(f"Done with {file_path}...")
 
     # If overwrite is true, overwrite the original .java file with the .ast content
     if overwrite:
-        with open(file_path+".ast", 'r', encoding='utf-8') as ast_file:
-            ast_content = ast_file.read()
-        with open(file_path, 'w', encoding='utf-8') as java_file:
-            java_file.write(ast_content)
-        os.remove(file_path+".ast")
+        file_path.unlink()
+        os.rename(file_path.with_suffix('.ast'), file_path)
         log.info("Original .java file overwritten with .ast content!")
-    else:
-        log.info("AST file saved!")
-
-
+    
+    log.info("File saved!")
 
 ## Tree traversal function
 def traverse(tree):
@@ -113,12 +136,17 @@ def traverse(tree):
         else:
             break
 
-## Processes a tree, compressed AST with hashing
-def process_tree_comp(tree):
-    lookup = {}
+# Process the tree into an AST
+def process_tree_ast(tree):
+    if tree is None:
+        raise ValueError("The tree object must not be None")
+
     lines = []
 
     def process_node(node):
+        if node is None:
+            raise ValueError("The tree object does not have the expected structure")
+
         line = ""
 
         if node.children:
@@ -129,7 +157,79 @@ def process_tree_comp(tree):
                     children.append(child_result)
             line = 'B {} {}'.format(node.type, ' '.join(map(str, children)))
         else:
-            text = node.text.decode('utf-8')
+            try:
+                text = node.text.decode('utf-8')
+            except UnicodeDecodeError:
+                raise ValueError("The text of the leaf nodes must be encoded using utf-8")
+            line = 'L {}'.format(text)
+
+        lines.append('{}'.format(line))
+
+        return len(lines) - 1
+
+    process_node(tree.root_node)
+    return '\n'.join(lines)
+
+## Processes the tree relatively
+def process_tree_ast_relatively(tree):
+    if tree is None:
+        raise ValueError("The tree object must not be None")
+
+    lines = []
+
+    def process_node(node):
+        if node is None:
+            raise ValueError("The tree object does not have the expected structure")
+
+        line = ""
+
+        if node.children:
+            children = []
+            for child in node.children:
+                child_result = process_node(child)
+                if child_result is not None:
+                    children.append(child_result)
+            line = 'B {} {}'.format(node.type, ' '.join(map(lambda x: str(x - len(lines)), children)))
+        else:
+            try:
+                text = node.text.decode('utf-8')
+            except UnicodeDecodeError:
+                raise ValueError("The text of the leaf nodes must be encoded using utf-8")
+            line = 'L {}'.format(text)
+
+        lines.append('{}'.format(line))
+
+        return len(lines) - 1
+
+    process_node(tree.root_node)
+    return '\n'.join(lines)
+
+## Processes a tree, compressed AST with hashing
+def process_tree_comp(tree):
+    if tree is None:
+        raise ValueError("The tree object must not be None")
+
+    lookup = {}
+    lines = []
+
+    def process_node(node):
+        if node is None:
+            raise ValueError("The tree object does not have the expected structure")
+
+        line = ""
+
+        if node.children:
+            children = []
+            for child in node.children:
+                child_result = process_node(child)
+                if child_result is not None:
+                    children.append(child_result)
+            line = 'B {} {}'.format(node.type, ' '.join(map(str, children)))
+        else:
+            try:
+                text = node.text.decode('utf-8')
+            except UnicodeDecodeError:
+                raise ValueError("The text of the leaf nodes must be encoded using utf-8")
             line = 'L {}'.format(text)
 
         idx = lookup.get(line)
@@ -145,69 +245,51 @@ def process_tree_comp(tree):
         return idx
 
     process_node(tree.root_node)
-    return '\n'.join(lines)        
-
-
-## Processes a tree and returns an AST 
-def process_tree(tree, code):
-    lines = []
-    def process_node(node):
-        if node.type == 'identifier':
-            lines.append(f'N {code[node.start_byte:node.end_byte].decode("utf-8")}')
-            lines.append(f'B identifier {len(lines) - 1}')
-            return len(lines) - 1
-        elif node.type == 'class':
-            return None
-        elif node.type == 'class_body':
-            children = []
-            for child in node.children:
-                if child.type not in ['{', '}']:
-                    child_index = process_node(child)
-                    if child_index is not None:
-                        children.append(child_index)
-            lines.append(f'B {node.type} {" ".join(map(str, children))}')
-            return len(lines) - 1
-        else:
-            children = []
-            for child in node.children:
-                child_index = process_node(child)
-                if child_index is not None:
-                    children.append(child_index)
-            lines.append(f'B {node.type} {" ".join(map(str, children))}')
-            return len(lines) - 1
-    process_node(tree.root_node)
     return '\n'.join(lines)
 
-## Processes the tree relatively
-def process_tree_relatively(tree, code):
+#Same as before, but this time sort hash values in the output
+def process_tree_comp_sorted(tree):
+    if tree is None:
+        raise ValueError("The tree object must not be None")
+
+    lookup = {}
     lines = []
-    def process_node(node):
-        if node.type == 'identifier':
-            lines.append(f'N {code[node.start_byte:node.end_byte].decode("utf-8")}')
-            lines.append(f'B identifier -1')
-            return len(lines) - 1
-        elif node.type == 'class':
-            return None
-        elif node.type == 'class_body':
+
+    def process_node_sorted(node):
+        if node is None:
+            raise ValueError("The tree object does not have the expected structure")
+
+        line = ""
+
+        if node.children:
             children = []
             for child in node.children:
-                if child.type not in ['{', '}']:
-                    child_index = process_node(child)
-                    if child_index is not None:
-                        children.append(child_index)
-            lines.append(f'B {node.type}')
-            return len(lines) - 1
+                child_result = process_node_sorted(child)
+                if child_result is not None:
+                    children.append(child_result)
+            line = 'B {} {}'.format(node.type, ' '.join(map(str, children)))
         else:
-            children = []
-            for child in node.children:
-                child_index = process_node(child)
-                if child_index is not None:
-                    children.append(child_index)
-            connections = [-len(lines) + i for i in children]
-            lines.append(f'B {node.type} {" ".join(map(str, connections))}')
-            return len(lines) - 1
-    process_node(tree.root_node)
-    return '\n'.join(lines)
+            try:
+                text = node.text.decode('utf-8')
+            except UnicodeDecodeError:
+                raise ValueError("The text of the leaf nodes must be encoded using utf-8")
+            line = 'L {}'.format(text)
+
+        idx = lookup.get(line)
+        if idx is not None:
+            return idx
+
+        hash_object = hashlib.sha256(line.encode('utf-8'))
+        hash_value = int.from_bytes(hash_object.digest()[:8], byteorder='big', signed=True)
+        idx = base64.urlsafe_b64encode(hash_value.to_bytes(8, byteorder='big', signed=True)).rstrip(b'=').decode('ascii')
+        lines.append('{} {}'.format(idx, line))
+        lookup[line] = idx
+
+        return idx
+
+    process_node_sorted(tree.root_node)
+    sorted_lines = sorted(lines, key=lambda x: x.split()[0])
+    return '\n'.join(sorted_lines)    
 
 ## Parse the java file and return the tree
 def parse_java_file(parser, file_path):
@@ -219,34 +301,16 @@ def parse_java_file(parser, file_path):
 
     tree_word = parser.parse(src.encode("utf8"))
     #tree_word = parser.parse(src)
-    
+
     return tree_word 
-
-
-## Save AST file
-# def save_ast_file(file_path, tree):
-#     with open(file_path, 'r') as f:
-#         src = f.read()
-#     with open(file_path+".ast", 'w') as f:
-#         f.write(process_tree(tree, src))
-
-def save_ast_file(file_path, tree):
-    with open(file_path, 'rb', encoding='utf-8') as f:
-        src_bytes = f.read()
-    with open(file_path + ".ast", 'wb', encoding='utf-8') as f:
-        f.write(process_tree(tree, src_bytes).encode('utf-8'))
-
 
 ## Save tokenized file
 def save_tokenized_file(file_path, tree):
-    with open(file_path+".ast",'w', encoding='utf-8') as f:
+    with open(file_path.with_suffix(".ast"),'w', encoding='utf-8') as f:
         for node in traverse(tree):
             if node.child_count == 0:
-                try:
                     f.write(node.text.decode('utf-8'))
-                except UnicodeDecodeError:
-                    continue
-                f.write("\n")
+                    f.write("\n")
 
 ## Check if the file is a java file
 def is_java_file(file_path):
